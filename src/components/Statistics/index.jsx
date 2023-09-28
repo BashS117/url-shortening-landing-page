38f8e33ea3551c826721549b0b95f644a6eea5f2
Sebastian Rodriguez <sebasrm2000@gmail.com>
2023-09-27 22:40:00 -0500

    styling Statistics Component

--- src/components/Statistics/index.jsx
+++ src/components/Statistics/index.jsx
@@ -1,23 +1,28 @@
 import React from 'react'
 import Shorten from '../../components/Shorten'
-
-
+import IconBrandRecognition from '../../assets/icon-brand-recognition.svg'
+import IconDetailedRecords from '../../assets/icon-detailed-records.svg'
+import IconFullyCustomizable from '../../assets/icon-fully-customizable.svg'
 const Statistics = () => {
   return (
 
 <section className='bg-[#ebebeb] px-[166px] '>
     <Shorten/>
     
-        <h1 class=" mt-[35px] mb-[5px] text-center text-2xl font-bold py-4 text-[2.5rem]">
+        <h1 class=" mt-[40px] mb-[5px] text-center text-2xl font-bold py-4 text-[2.5rem]">
         Advanced Statistics
         </h1>
         <p className='px-[280px] text-[1.2rem] text-[var(--Grayish-Violet)] leading-9'>Track how your links are performing across the web with our 
   advanced statistics dashboard.</p>
 
-          <div class="grid justify-center items-center text-start grid-cols-3 mt-[55px] gap-[30px]">
-
-              <div class="pt-16 rounded overflow-hidden shadow-lg  bg-[var(--Off-white)]">
+          <div class="relative grid justify-center items-center text-start grid-cols-3 mt-[10px] gap-[25px]">
+          <hr className="bg-[var(--Cyan)] h-3 w-full absolute mt-[30px]" />
+              <div class="relative pt-16 pb-[25px] rounded  shadow-lg  bg-[var(--Off-white)]">
                   <div class="px-6 py-4">
+                      <div className="absolute top-[-2rem] w-[90px] h-[90px] flex items-center justify-center rounded-full bg-[var(--Dark-Violet)]">
+                        <img src={IconBrandRecognition} alt="" />
+                      </div>
+
                       <h3 class="font-bold text-[1.4rem] mb-2">Brand Recognition</h3>
                       <p class="text-[var(--Grayish-Violet)] text-base leading-7">
                           Boost your brand recognition with each click. Generic links don’t
@@ -26,8 +31,12 @@ const Statistics = () => {
                   </div>
               </div>
 
-              <div class="pt-16 rounded overflow-hidden shadow-lg bg-[var(--Off-white)] mt-[80px] ">
+              <div class="pt-16 pb-[25px] rounded relative  shadow-lg bg-[var(--Off-white)] mt-[80px] ">
                   <div class="px-6 py-4">
+
+                      <div className="absolute top-[-2rem] w-[90px] h-[90px] flex items-center justify-center rounded-full bg-[var(--Dark-Violet)]">
+                        <img src={IconDetailedRecords} alt="" />
+                      </div>
                       <h3 class="font-bold text-[1.4rem] mb-2">Detailed Records</h3>
                       <p class="text-[var(--Grayish-Violet)] text-base leading-7">
                           Gain insights into who is clicking your links. Knowing when and where
@@ -36,8 +45,11 @@ const Statistics = () => {
                   </div>
               </div>
 
-              <div class="pt-16 rounded overflow-hidden shadow-lg  bg-[var(--Off-white)] mt-[160px]">
+              <div class="pt-16 pb-[25px] rounded relative shadow-lg  bg-[var(--Off-white)] mt-[160px]">
                   <div class="px-6 py-4">
+                  <div className="absolute top-[-2rem] w-[90px] h-[90px] flex items-center justify-center rounded-full bg-[var(--Dark-Violet)]">
+                        <img src={IconFullyCustomizable} alt="" />
+                      </div>
                       <h3 class="font-bold text-[1.4rem] mb-2">  Fully Customizable
                       </h3>
                       <p class="text-[var(--Grayish-Violet)] text-base leading-7">
